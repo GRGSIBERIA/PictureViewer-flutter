157dd2d78e9b3ab222011de2834dfc4c7a5bcf2b
Eiichi Takebuchi <nanashi4129@gmail.com>
2021-08-12 18:03:35 +0900

    add picture and thumbnail scheme

--- picture_viewer/lib/scheme.dart
+++ picture_viewer/lib/scheme.dart
@@ -8,13 +8,15 @@ class Picture {
   final int width;
   final int height;
   final int size_bytes;
+  final int bytes;
 
   Picture(
       {required this.id,
       required this.digest,
       required this.width,
       required this.height,
-      required this.size_bytes});
+      required this.size_bytes,
+      required this.bytes});
 
   Map<String, dynamic> toMap() {
     return {
@@ -22,7 +24,8 @@ class Picture {
       'digest': digest,
       'width': width,
       'height': height,
-      'size_bytes': size_bytes
+      'size_bytes': size_bytes,
+      'bytes': bytes
     };
   }
 
@@ -34,14 +37,30 @@ class Picture {
 
 class Thumbnail {
   final int picture_id;
+  final String digest;
+  final int width;
+  final int height;
+  final int size_bytes;
   final Uint8List bytes;
 
-  Thumbnail({required this.picture_id, required this.bytes});
+  Thumbnail(
+      {required this.picture_id,
+      required this.digest,
+      required this.width,
+      required this.height,
+      required this.size_bytes,
+      required this.bytes});
 
   Map<String, dynamic> toMap() {
-    return {'picture_id': picture_id, 'data': bytes};
+    return {'picture_id': picture_id, 'bytes': bytes};
+  }
+
+  @override
+  String toString() {
+    return 'Thumbnail{picture_id: $picture_id, digest: $digest, size_bytes: $size_bytes}';
   }
 }
+
 
 /**
  * Futer<ByteData> Asset::getByteData()
